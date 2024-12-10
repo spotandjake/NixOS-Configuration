4077d9c406ea03e15948f5bb08f08f9d07f8a47c
Spotandjake <spotandjake@hotmail.com>
2024-12-10 01:03:23 -0500

    feat: Initial commit

--- flake.nix
+++ flake.nix
@@ -1,65 +1,33 @@
 {
-  description = "Spotandjake Flake";
-  # Nix Sources
   inputs = {
-    # Official NixOS repo
-    master.url = "github:NixOS/nixpkgs/master";
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    stable.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # Current nixpkgs branch
-    nixpkgs.follows = "unstable";
-    # NixOS community
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    # impermanence.url = "github:/nix-community/impermanence";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-    };
-    # MacOS configuration
-    darwin = {
-      url = "github:LnL7/nix-darwin/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    # Security
-    sops-nix.url = "github:Mic92/sops-nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
   };
-  # Nix Script
+
   outputs =
-    {
-      self,
-      flake-parts,
-      nixpkgs,
-      ...
-    }@inputs:
+    { systems, nixpkgs, ... }@inputs:
     let
-      config = import ./config.nix;
-      # Import The Generator library
-      gen = import ./lib { inherit self inputs; inherit (nixpkgs) lib; };
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
     in
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      # Generate The Parts
-      systems = gen.forAllSystems;
-      # Load Nix Parts Scripts
-      imports = [ ./parts ];
-      # Generate The Hosts
-      flake = {
-        # Operating System Configurations
-        nixosConfigurations = gen.mkHosts {
-          inherit config;
-          platform = "nixos";
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs-22_x
+
+            # You can set the major version of Node.js to a specific one instead
+            # of the default version
+            # pkgs.nodejs-22_x
+
+            # Comment out one of these to use an alternative package manager.
+            pkgs.yarn
+            # pkgs.pnpm
+            # pkgs.bun
+
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
         };
-        darwinConfigurations = gen.mkHosts {
-          inherit config;
-          platform = "darwin";
-        };
-        # Flake Templates
-        templates = import "${self}/templates" { inherit self; };
-      };
+      });
     };
 }
--- home/modules/zsh/.zshrc
+++
@@ -1,8 +0,0 @@
-# export PATH="${PATH}:$(yarn global bin)"
-# export PATH="/opt/homebrew/bin:$PATH"
-# export PATH="/Users/spotandjake/Library/Caches/fnm_multishells/3219_1677859702535/bin:$PATH"
-
-# Fnm
-# eval "$(fnm env --use-on-cd)"
-
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
