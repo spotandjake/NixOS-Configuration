f68ae69fea5517e30e8ac84d6c55a65647f263bb
Spotandjake <spotandjake@hotmail.com>
2024-12-11 14:59:58 -0500

    feat: Improve installer

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-nix run nix-darwin -- switch --flake . --show-trace
+nix --extra-experimental-features nix-command --extra-experimental-features flakes run nix-darwin -- switch --flake . --show-trace
 # nix run cachix authtoken XXXX
 # nix run cachix push spotandjakeflake
