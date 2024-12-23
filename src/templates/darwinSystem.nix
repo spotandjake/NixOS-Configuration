f560dc2abcefdd74d62c22f96f41f5337310a875
Spotandjake <spotandjake@hotmail.com>
2024-12-22 23:40:10 -0500

    feat: 1password cli

--- src/templates/darwinSystem.nix
+++ src/templates/darwinSystem.nix
@@ -45,7 +45,7 @@ let
       # This is a module from nix-darwin
       # Homebrew is *installed* via the flake input nix-homebrew
       enable = true;
-      casks = [ "lunar" "1Password" ];
+      casks = [ "lunar" "1Password" "1password-cli" ];
 
       # These app IDs are from using the mas CLI app
       # mas = mac app store
