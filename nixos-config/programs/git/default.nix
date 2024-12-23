2e2818fe8edf6b6fdae264e2351310e5b7e98089
Spotandjake <spotandjake@hotmail.com>
2024-12-22 23:21:00 -0500

    feat: 1password commit sign

--- nixos-config/programs/git/default.nix
+++ nixos-config/programs/git/default.nix
@@ -18,6 +18,10 @@ in {
         signByDefault = true;
         gpgPath = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
       };
+      extraConfig = {
+        gpg.format = "ssh";
+        gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
       aliases = {
         lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
         lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
